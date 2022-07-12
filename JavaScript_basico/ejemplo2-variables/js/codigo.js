179d636ee92de16d0b017159ce720fb91c9635ba
AinhoaSalo <94933683+AinhoaSalo@users.noreply.github.com>
2022-07-12 14:52:52 +0200

    Add class 12/07/22

---
+++ JavaScript_basico/ejemplo2-variables/js/codigo.js
@@ -0,0 +1,18 @@
+let cadena = "Ainhoa";
+let numero = 31;
+let booleano = true;
+let objeto = {};
+let objetoDate = new Date();
+let sinDeclarar;
+
+console.log( typeof cadena)
+console.log( typeof numero)
+console.log( typeof booleano)
+console.log( typeof objeto)
+console.log( typeof objetoDate)
+console.log( typeof sinDeclarar)
+
+document.getElementById("string").innerHTML= `Hola, me llamo ${cadena}` 
+document.getElementById("number").innerHTML= `Tengo ${numero} años`
+
+
